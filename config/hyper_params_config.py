69c2d44813fc874c29dd93154ef668518efaeee7
zzs20020430 <zzs20020430@126.com>
2024-11-28 22:30:39 +0800

    Update hyper_params_config.py
    adjust task weights

--- config/hyper_params_config.py
+++ config/hyper_params_config.py
@@ -5,8 +5,12 @@ classification_loss_w=1
 binary_classification_loss_w=0.2
 modal_contrastive_loss_w=0.2
 aug_classification_loss_w=1
-aug_text_loss_w=0.2
-aug_audio_loss_w=0.2
+aug_text_recon_loss_w=0.2
+aug_audio_recon_loss_w=0.2
+
+audio_kl_w=1
+text_kl_w=1
+
 # Instantiate models
 num_labels = 4
 batch_size=4
